3c163f4f16fb2e72298addbb25de311eab8fd88d
murat-boz <muratboz@hotmail.com>
2023-10-03 00:14:22 +0300

    Added dictionart extension methods.

--- UsefulMethods/Extensions/IEnumerableExtensions.cs
+++ UsefulMethods/Extensions/IEnumerableExtensions.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace UsefulMethods.EnumerableExtensions
+namespace UsefulMethods.Extensions
 {
     public static class IEnumerableExtensions
     {
